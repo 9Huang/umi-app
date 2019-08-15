0fba521b981a23e8d5289c68fe22d467846d2339
huangj <15150599658@163.com>
2019-08-15 16:26:30 +0800

    路由配置

---
+++ config/router.js
@@ -0,0 +1,121 @@
+export default {
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/BasicLayout',
+      Routes: ['src/pages/Authorized'],
+      authority: ['admin', 'user'],
+      routes: [
+        {
+          path: '/',
+          name: 'welcome',
+          icon: 'smile',
+          component: './Welcome',
+        },
+        {
+          path: '/template/dashboard/*',
+          name: 'dashboard',
+          icon: 'dashboard',
+          routes: [
+            {
+              name: '分析页',
+              path: '/template/dashboard/analysis',
+              component: './template/dashboard/analysis',
+            },
+            {
+              name: '监控页',
+              path: '/template/dashboard/monitor',
+              component: './template/dashboard/monitor',
+            },
+            {
+              name: '工作台',
+              path: '/template/dashboard/workplace',
+              component: './template/dashboard/workplace',
+            },
+          ],
+        },
+        {
+          path: '/template/list/*',
+          name: '列表页',
+          icon: 'table',
+          routes: [
+            {
+              path: '/template/list/search/*',
+              name: '搜索列表',
+              routes: [
+                {
+                  name: '文章',
+                  path: '/template/list/search/articles',
+                  component: './template/list/search/articles',
+                  exact: true,
+                },
+              ],
+            },
+            {
+              name: '查询表格',
+              path: '/template/list/table-list',
+              component: './template/list/table-list',
+            },
+            {
+              name: '标准列表',
+              path: '/template/list/basic-list',
+              component: './template/list/basic-list',
+            },
+            {
+              name: '卡片列表',
+              path: '/template/list/card-list',
+              component: './template/list/card-list',
+            },
+          ],
+        },
+        {
+          path: '/template/profile/*',
+          name: '详情页',
+          icon: 'profile',
+          routes: [
+            {
+              name: '基础详情页',
+              path: '/template/profile/basic',
+              component: './template/profile/basic',
+            },
+            {
+              name: '高级详情页',
+              path: '/template/profile/advanced',
+              component: './template/profile/advanced',
+            },
+          ],
+        },
+        {
+          path: '/finance/*',
+          name: '财务',
+          icon: 'pay-circle',
+          routes: [
+            {
+              path: '/finance/wallet/*',
+              name: '钱包',
+              icon: 'wallet',
+              routes: [
+                {
+                  name: '钱包分类列表',
+                  path: '/finance/wallet/wallet-class-list',
+                  component: './finance/wallet/wallet-class-list',
+                },
+                {
+                  name: '钱包类型列表',
+                  path: '/finance/wallet/wallet-type-list',
+                  component: './finance/wallet/wallet-type-list',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          component: './404',
+        },
+      ],
+    },
+    {
+      component: './404',
+    },
+  ],
+};
